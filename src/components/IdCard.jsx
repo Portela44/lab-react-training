d7246d6b490431957076e06b0aafe76eb6357552
Guillem <guillem.mart7@gmail.com>
2022-07-24 18:36:58 +0200

    iteration 1 done

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function IdCard(props) {
+    const {lastName, firstName, gender, height, birth, picture} = props;
+    console.log(props);
+    return (
+        <div className="card">
+            <img src={picture} alt="profile" />
+            <div className="cardInformation">
+                <p><span className="infoTitle">First Name: </span>{firstName}</p>
+                <p><span className="infoTitle">Last Name: </span>{lastName}</p>
+                <p><span className="infoTitle">Gender: </span>{gender}</p>
+                <p><span className="infoTitle">Height: </span>{height}</p>
+                <p><span className="infoTitle">Birth: </span>{birth}</p>
+            </div>
+        </div>
+    )
+}
